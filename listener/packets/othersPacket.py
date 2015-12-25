513add94cd75ff459971c949b5395e685fe6c05b
Mishk0 <mshelemetev@gmail.com>
2015-12-25 13:19:16 -0500

    the dummy others protocol

--- listener/packets/othersPacket.py
+++ listener/packets/othersPacket.py
@@ -1,5 +1,16 @@
 from listener.packets.abstractPacket import AbstractPacket
 
 class OthersPacket(AbstractPacket):
-    def __init__(self):
+   
+    def __init__(self, binPacket: bytes, margin: int):
+        self.binPacket = binPacket
+        self.headerMargin = margin
+        self.toPort = 'others'
+        self.fromPort = 'others'
+        self.parse()
+    
+    def parse(self):
         pass
+    
+    def getName(self):
+        return 'N/A protocol'
